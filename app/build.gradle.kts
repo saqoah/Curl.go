625df394dfdee272d41fd533914d9c5451ecffb7
hmz <66381391+saqoah@users.noreply.github.com>
2026-03-29 22:15:08 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,10 +44,6 @@ android {
         jvmToolchain(17)
     }
 
-    buildFeatures {
-        compose = true
-    }
-
     packaging {
         resources { excludes += "/META-INF/{AL2.0,LGPL2.1}" }
     }
